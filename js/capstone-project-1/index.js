c207c932946228637a93516b7dc2159e2fc3dac4
Joywin Bennis <107112207+joywin2003@users.noreply.github.com>
2024-09-03 22:59:54 +0530

    added custom validation

--- js/capstone-project-1/index.js
+++ js/capstone-project-1/index.js
@@ -1,11 +1,42 @@
-let form = document.getElementById("form");
+const name = document.getElementById("name");
+const email = document.getElementById("email");
+const password = document.getElementById("password");
+const dob = document.getElementById("dob");
+const acceptTerms = document.getElementById("acceptTerms");
 
-let formSubmit = (e) => {
-  e.preventDefault();
-  const name = document.getElementById("name").value;
-  const email = document.getElementById("email").value;
-  const password = document.getElementById("password").value;
-  const dob = document.getElementById("dob").value;
-  const acceptedTermsAndconditions = document.getElementById("acceptTerms").checked;
-  console.log(name, email, password, dob, acceptedTermsAndconditions);
+console.log(name.value);
+
+const validate = (element, message) => {
+  if (element.validity.valueMissing || element.validity.typeMismatch) {
+    element.setCustomValidity(message);
+  } else {
+    element.setCustomValidity("");
+  }
+  element.reportValidity();
 };
+
+email.addEventListener("input", () => {
+  validate(email, "Please enter a valid email address.");
+});
+name.addEventListener("input", () => {
+  validate(name, "Name is required.");
+});
+password.addEventListener("input", () => {
+  validate(password, "Password is required.");
+});
+dob.addEventListener("input", () => {
+  validate(dob, "Date of birth is required.");
+});
+acceptTerms.addEventListener("input", () => {
+  validate(acceptTerms, "Please accept the terms and conditions.");
+});
+
+document.getElementById("user-form").addEventListener("submit", function (e) {
+  e.preventDefault();
+
+  if (document.getElementById("user-form").checkValidity()) {
+    console.log("Form submitted successfully!");
+  } else {
+    console.log("Form not submitted successfully!");
+  }
+});
