7629f2906d2bb6e7a32d5a8346108e604330fbac
Joywin Bennis <107112207+joywin2003@users.noreply.github.com>
2024-09-03 23:30:21 +0530

    Feat: Refactor form validation function

--- js/capstone-project-1/index.js
+++ js/capstone-project-1/index.js
@@ -4,8 +4,6 @@ const password = document.getElementById("password");
 const dob = document.getElementById("dob");
 const acceptTerms = document.getElementById("acceptTerms");
 
-console.log(name.value);
-
 const validate = (element, message) => {
   if (element.validity.valueMissing || element.validity.typeMismatch) {
     element.setCustomValidity(message);
@@ -18,9 +16,11 @@ const validate = (element, message) => {
 email.addEventListener("input", () => {
   validate(email, "Please enter a valid email address.");
 });
+
 name.addEventListener("input", () => {
   validate(name, "Name is required.");
 });
+
 password.addEventListener("input", () => {
   validate(password, "Password is required.");
 });
@@ -34,40 +34,52 @@ const minDate = new Date(
   today.getMonth(),
   today.getDate()
 );
+
 const maxDate = new Date(
   today.getFullYear() - minAge,
   today.getMonth(),
   today.getDate()
 );
 
-const dobInput = document.getElementById("dob");
 const formatDate = (date) => date.toISOString().split("T")[0];
-dobInput.setAttribute("min", formatDate(minDate));
-dobInput.setAttribute("max", formatDate(maxDate));
 
-dobInput.addEventListener("input", () => {
-  console.log(new Date(dob.value), new Date(minDate));
+dob.setAttribute("min", formatDate(minDate));
+dob.setAttribute("max", formatDate(maxDate));
+
+dob.addEventListener("input", () => {
+  const dobValue = new Date(dob.value);
+
   if (dob.value === "") {
-    dobInput.setCustomValidity("Date of birth is required.");
-  } else if (
-    new Date(dob.value) < new Date(minDate) ||
-    new Date(dob.value) > new Date(maxDate)
-  ) {
-    dobInput.setCustomValidity(`Date of birth must be between 18 and 55.`);
+    dob.setCustomValidity("Date of birth is required.");
+  } else if (dobValue < minDate || dobValue > maxDate) {
+    dob.setCustomValidity("Date of birth must be between 18 and 55 years old.");
   } else {
-    dobInput.setCustomValidity("");
+    dob.setCustomValidity("");
   }
+
+  dob.reportValidity();
 });
 
-document.getElementById("user-form").addEventListener("submit", function (e) {
-  e.preventDefault();
+acceptTerms.addEventListener("input", () => {
   if (!acceptTerms.checked) {
     acceptTerms.setCustomValidity("You must accept the terms and conditions.");
   } else {
     acceptTerms.setCustomValidity("");
   }
   acceptTerms.reportValidity();
-  if (document.getElementById("user-form").checkValidity()) {
+});
+
+document.getElementById("user-form").addEventListener("submit", function (e) {
+  e.preventDefault();
+
+  if (!acceptTerms.checked) {
+    acceptTerms.setCustomValidity("You must accept the terms and conditions.");
+  } else {
+    acceptTerms.setCustomValidity("");
+  }
+  acceptTerms.reportValidity();
+
+  if (this.checkValidity()) {
     console.log("Form submitted successfully!");
   } else {
     console.log("Form not submitted successfully!");
